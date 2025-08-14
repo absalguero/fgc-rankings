7ec36c7ffff2e30930cf212ac646708b62ea5fdd
Alex Salguero <224894938+absalguero@users.noreply.github.com>
2025-08-13 22:05:07 -0700

    Add Ranking Change

--- _data/rankings.js
+++ _data/rankings.js
@@ -1,5 +1,5 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
-const Papa = require("papaparse");
+const { parse } = require('csv-parse/sync');
 
 module.exports = async function() {
   const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS-5EA7Uvl16qJolXsMR4SRAzXb6XwclnVtT92byBa34nk3WMk6qL2XG79-xF-uRg/pub?gid=1862929315&single=true&output=csv';
@@ -15,11 +15,22 @@ module.exports = async function() {
     }
   });
 
-  // Parse the CSV data
-  const { data } = Papa.parse(csvText, {
-    header: true,
-    skipEmptyLines: true,
+  // Parse the CSV data with csv-parse
+  const data = parse(csvText, {
+    columns: true,
+    skip_empty_lines: true,
   });
 
-  return { players: data.slice(0, 25) };
+  // Map the raw data to a clean array of objects with Nunjucks-friendly keys
+  const players = data.map(row => ({
+    Rank: row['Rank'],
+    Rank_Change: row['Rank Change'],
+    Player: row['Player'],
+    Character: row['Main Character'],
+    Region: row['Region'],
+    Points: row['Points'],
+  }));
+
+  // Return the data with the 'players' key, limited to 25 players
+  return { players: players.slice(0, 25) };
 };
